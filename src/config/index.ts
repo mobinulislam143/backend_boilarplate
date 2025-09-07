a7bb6e5586bed7780bde65b9f393e0ae2e85059c
mobinulislam143 <mobinulislammahi@gmail.com>
2025-09-07 19:57:00 +0600

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import path from "path"
+
+dotenv.config({ path: path.join(process.cwd(), ".env") })
+
+export default {
+  env: process.env.NODE_ENV,
+  stripe_key: process.env.STRIPE_SECRET_KEY,
+  port: process.env.PORT,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    expires_in: process.env.EXPIRES_IN || "7d",
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_TOKEN,
+    reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+  },
+  reset_pass_link: process.env.RESET_PASS_LINK,
+  DO_SPACES_ENDPOINT: process.env.DO_SPACES_ENDPOINT,
+  DO_ACCESS_KEY_ID: process.env.DO_SPACES_ACCESS_KEY,
+  DO_SECRET_ACCESS_KEY: process.env.DO_SPACES_SECRET_KEY,
+  DO_SPACES_BUCKET: process.env.DO_SPACES_BUCKET,
+
+
+
+  admin_email: process.env.ADMIN_EMAIL,
+  admin_password: process.env.ADMIN_PASSWORD,
+}
